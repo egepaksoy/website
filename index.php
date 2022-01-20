0348aee2f033ed6406a8b0fac3c61544ee337a71
Ege Paksoy <egepaskoy@gmail.com>
2022-01-20 20:57:00 +0300

    page

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-echo "hello world!"
-?>
+<html>
+<head>
+</head>
+<body>
+<div style="text-align: center">
+<h1>Ege Paksoy</h1>
+<h4 style="opacity: 0.7; font-style: italic;">Student, software developer</h4>
+</div>
+</body>
